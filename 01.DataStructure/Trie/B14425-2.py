ea810de0f34df5d11d86d25793e435896de7823c
geun9716 <crintop@gmail.com>
2022-03-15 18:49:54 +0900

    Trie startWith 추가

--- 01.DataStructure/Trie/B14425-2.py
+++ 01.DataStructure/Trie/B14425-2.py
@@ -25,6 +25,14 @@ class Trie:
             node = node.children[ch]
         return node.word
 
+    def startsWith(self, prefix:str):
+        node = self.root
+        for ch in prefix:
+            if ch not in node.children:
+                return False
+            node = node.children[ch]
+        return True
+        
 def sol():
     N, M = map(int, sys.stdin.readline().strip().split(" "))
     answer = 0
