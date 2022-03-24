b39fd8ac59bef695ca96bf260b08b22b859b29c7
geun9716 <crintop@gmail.com>
2022-03-24 16:53:10 +0900

    [BOJ22862] 투포인터 (완)

--- 11.TwoPointer/B22862.py
+++ 11.TwoPointer/B22862.py
@@ -8,20 +8,21 @@ l, r = 0, 0
 maxValue = 0
 cnt = 0
 while l <= r and r < N:
-    print(l, r, cnt)
-    print(S[l:r])
-    if cnt < K: 
+    if cnt < K:
         if S[r] % 2 != 0: 
             cnt += 1
         r += 1
-        maxValue = max(maxValue, r-l)
+        maxValue = max(maxValue, r-l-cnt)
     else:
         if S[r] % 2 == 0: # cnt가 K일때 짝수가 들어오는 경우
             r += 1
-            maxValue = max(maxValue, r-l)
+            maxValue = max(maxValue, r-l-cnt)
         else: #홀수가 들어오는 경우
             if S[l] % 2 != 0: #S[l]을 제거
                 cnt -= 1
             l += 1
+    
+    # print(l, r, cnt)
+    # print(S[l:r])
 
-print(maxValue-K)
+print(maxValue)
