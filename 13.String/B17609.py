10ef3453837b6bb3eca0a86f7f830672897c33ab
geun9716 <crintop@gmail.com>
2022-04-07 14:54:07 +0900

    [BOJ17609] 회문 - 투포인터

--- 13.String/B17609.py
+++ 13.String/B17609.py
@@ -1,11 +1,22 @@
 import sys
 input = sys.stdin.readline
 
+def isPalindrome(string, l, r, UseDelete):
+    IsPseudoPalindrome = True
+    while 0 <= l and r < len(string) and l <= r:
+        if string[l] == string[r]:
+            l += 1
+            r -= 1
+        else:
+            if not UseDelete:
+                return isPalindrome(string, l+1, r, True) | isPalindrome(string, l, r-1, True)
+            else:
+                return False
+    return IsPseudoPalindrome
+
 for _ in range(int(input())):
     string = input().strip()
-    print(string,"=================")
-    IsPseudoPalindrome = True
-    UseDelete = False
+    # print(string,"=================")
     # Palindrome
     if string == string[::-1]:
         print(0)
@@ -13,15 +24,8 @@ for _ in range(int(input())):
 
     # Is Psuedo Palindrome?
     # Devide half string
-    mid = len(string) // 2
-    str1 = string[0:mid]
-    str2 = string[mid:]
-
-    print(str1)
-    print(str2)
-
-
-    if IsPseudoPalindrome:
+    result = isPalindrome(string, 0, len(string)-1, False)
+    if result:
         print(1)
     else:
         print(2)
